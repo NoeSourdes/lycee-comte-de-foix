2f3d5a85d7e2fb2b8c09e80153d7792e3f408fd8
NoeSourdes <sourdesnoe19@gmail.com>
2024-12-17 00:05:15 +0100

    Add social media buttons to contact form and adjust layout for better presentation

--- components/contact/Form.tsx
+++ components/contact/Form.tsx
@@ -1,4 +1,7 @@
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { RiTwitterXLine } from "react-icons/ri";
 import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 import { ComponentForm } from "./ComponentForm";
 
@@ -17,8 +20,21 @@ export const Form = () => {
           suivantes afin que nous puissions mieux comprendre vos besoins :
         </p>
       </div>
-      <Card className="bg-background p-8">
-        <ComponentForm />
+      <Card className="bg-background p-8 relative">
+        <div className="flex items-center gap-3 absolute -top-5 left-1/2 -translate-x-1/2">
+          <Button size="icon" className="bg-destructive text-background">
+            <FaFacebookF />
+          </Button>
+          <Button size="icon" className="bg-destructive text-background">
+            <RiTwitterXLine />
+          </Button>
+          <Button size="icon" className="bg-destructive text-background">
+            <FaLinkedinIn />
+          </Button>
+        </div>
+        <div className="mt-5">
+          <ComponentForm />
+        </div>
       </Card>
     </div>
   );
